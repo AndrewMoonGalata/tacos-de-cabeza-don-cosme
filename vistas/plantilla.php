d6ab3c3713690ebdfb2b9d8e845e30a8131cf212
AndrewMoonGalata <a3519110190@alumno.uttehuacan.edu.mx>
2023-03-14 23:23:31 -0600

    funcion editar y eliminar categoria

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -142,6 +142,7 @@ session_start();
 
 <script src="vistas/js/plantilla.js"></script>
 <script src="vistas/js/usuarios.js"></script>
+<script src="vistas/js/categorias.js"></script>
 
 </body>
 </html>
